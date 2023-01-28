e3b1ee43f341bcd063438215b45a9d56f92aba8c
deepaksorout <39903636+deepaksorout@users.noreply.github.com>
2023-01-28 16:50:07 +0530

    s

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -26,4 +26,72 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+
+    public String createUser(String name,String number){
+        User user=new User(name,number);
+        if(userMobile.contains(number)){
+            return "";
+        }
+        userMobile.add(number);
+        return"User Successfully created";
+    }
+
+    public Group createUser(List<User> users) {
+        int n=users.size();
+        if(n==2){
+            Group group=new Group(users.get(1).getName(),n);
+            groupUserMap.put(group,users);
+            adminMap.put(group,users.get(0));
+            return group;
+        }
+        if(n>2){
+            customGroupCount+=1;
+            Group group=new Group("Group "+customGroupCount,n);
+            groupUserMap.put(group,users);
+            adminMap.put(group,users.get(0));
+            return group;
+        }
+        return null;
+    }
+
+    public int createMessage(String content) {
+        messageId+=1;
+        Message message=new Message(messageId,content,new Date());
+        return messageId;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) {
+        if(!groupUserMap.containsKey(group)){
+            return -1;
+        }
+        if(!groupUserMap.get(group).contains(sender)){
+            return -2;
+        }
+        senderMap.put(message,sender);
+        if(!groupMessageMap.containsKey(group)){
+            List<Message> list=new ArrayList<>();
+            list.add(message);
+            groupMessageMap.put(group,list);
+        }
+        else {
+            List<Message> list=groupMessageMap.get(group);
+            list.add(message);
+            groupMessageMap.put(group,list);
+        }
+        return groupMessageMap.get(group).size();
+    }
+
+    public String changeAdmin(User approver, User user, Group group) {
+        if(!groupUserMap.containsKey(group)){
+            return "";
+        }
+        if(!adminMap.get(group).equals(approver)){
+            return "";
+        }
+        if(!groupUserMap.get(group).contains(user)){
+            return "";
+        }
+        adminMap.put(group,user);
+        return"Successfully admin changed";
+    }
 }
